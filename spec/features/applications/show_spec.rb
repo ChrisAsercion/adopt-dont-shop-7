196a38ba1153ee6732cf0631499b3bf5fce4b65a
Chris Cullinane <chriscullinane@protonmail.com>
2023-07-13 14:38:37 -0800

    show test

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,9 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "Application show" do
+  before :each do
+    @application_1 = Application.create!(name: "Example app", address: "123 sesame st. New York, NY 10001", description: "I want dog", status: "Accepted")
+  end
+
   it 'page has application data' do
-    application_1 = Application.create!(name: "Example app", address: "123 sesame st. New York, NY 10001", description: "I want dog", status: "Accepted")
-    visit "/application/#{application_1.id}"
+    visit "/applications/#{@application_1.id}"
+    save_and_open_page
     expect(page).to have_content("Name: Example app")
+    expect(page).to have_content("Address: 123 sesame st. New York, NY 10001")
+    expect(page).to have_content("Description: I want dog")
+    expect(page).to have_content("Status: Accepted")
   end
 end

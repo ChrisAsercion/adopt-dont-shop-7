202b2ef479f72d652d144630217f120a765c1e3e
Chris Cullinane <chriscullinane@protonmail.com>
2023-07-14 15:47:48 -0800

    half way there

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Application show" do
     @shelter = Shelter.create!(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
     @pet = Pet.create!(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: @shelter.id)
     @application_1 = Application.create!(name: "Topher C.", address: "123 sesame st. New York, NY 10001", description: "I want dog", status: "Accepted")
-    @application_1.pets << @pet
+    #@application_1.pets << @pet
 
   end
 
@@ -17,5 +17,12 @@ RSpec.describe "Application show" do
     expect(page).to have_content("Description: I want dog")
     expect(page).to have_content("Status: Accepted")
     expect(page).to have_link("Scooby")
+  end
+  # User Story 4
+  it 'page has pets search form' do
+    visit "/applications/#{@application_1.id}"
+    save_and_open_page
+    expect(page).to have_content("Add a Pet to this Application")
+    expect(page).to have_field("Search")
   end
 end

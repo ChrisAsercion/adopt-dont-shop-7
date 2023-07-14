f12513556c08017c4bcaf2449a2f59c4dc158d38
Chris Cullinane <chriscullinane@protonmail.com>
2023-07-14 03:25:19 -0800

    pets in applications are links on show

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,15 +2,20 @@ require "rails_helper"
 
 RSpec.describe "Application show" do
   before :each do
-    @application_1 = Application.create!(name: "Example app", address: "123 sesame st. New York, NY 10001", description: "I want dog", status: "Accepted")
+    @shelter = Shelter.create!(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
+    @pet = Pet.create!(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: @shelter.id)
+    @application_1 = Application.create!(name: "Topher C.", address: "123 sesame st. New York, NY 10001", description: "I want dog", status: "Accepted")
+    @application_1.pets << @pet
+
   end
 
   it 'page has application data' do
     visit "/applications/#{@application_1.id}"
-    # save_and_open_page
-    expect(page).to have_content("Name: Example app")
+    save_and_open_page
+    expect(page).to have_content("Name: Topher C.")
     expect(page).to have_content("Address: 123 sesame st. New York, NY 10001")
     expect(page).to have_content("Description: I want dog")
     expect(page).to have_content("Status: Accepted")
+    expect(page).to have_link("Scooby")
   end
 end

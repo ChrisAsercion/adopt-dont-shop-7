7161ec3a7df63a668b532b52bcc7820e8f73c0b5
Chris Cullinane <chriscullinane@protonmail.com>
2023-07-18 16:36:59 -0800

    no more shovels

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -6,7 +6,6 @@ class PetApplicationsController < ApplicationController
     application = Application.find(params[:application_id])
     pet = Pet.find(params[:pet_id])
     PetApplication.create!(pet_id: pet.id, application_id: application.id )
-    application.pets << pet #this should be created in a table (look for AR)
     redirect_to "/applications/#{application.id}"
   end
 

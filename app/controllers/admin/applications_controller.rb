b09456a22a6e0db2e6dd2ad97e1ff6d26ffcf306
Chris Cullinane <chriscullinane@protonmail.com>
2023-07-20 09:03:35 -0800

    starting to fix admin show page

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,6 +1,10 @@
 class Admin::ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
-    @pets = @application.pets
+    @pending_pets = @application.pending_pets
+    @rejected_pets = @application.rejected_pets
+    @approved_pets = @application.approved_pets
+
+    # @pets = @application.pets
   end
 end
--- app/models/application.rb
+++ app/models/application.rb
@@ -8,4 +8,16 @@ class Application < ApplicationRecord
   def pet_status(pet_id)
     pet_applications.where(pet_id: pet_id).first
   end
+
+  def pending_pets
+    pets.where(applications_pets: { pet_status: 'pending' })
+  end
+
+  def approved_pets
+    pets.where(applications_pets: { pet_status: 'accepted' })
+  end
+
+  def rejected_pets
+    pets.where(applications_pets: { pet_status: 'rejected' })
+  end
 end 
